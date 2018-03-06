e99a3626e39d53c2f20a38a34af764f50964814d
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-06 01:00:47 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -79,9 +79,8 @@ def can_update_multiple_items_at_once
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
   end
-  binding.pry
+  # binding.pry
   Movie.where('release_date >= 2000')
-
 
 end
 
