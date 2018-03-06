71a54daed40278e41be5ce72bc3a3c515d2bf3e1
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-06 01:09:20 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -84,8 +84,7 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  binding.pry
-
+  # binding.pry
   Movie.destroy(Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once").id)
 end
 
